46fdbe32381ade91abfe157162a391e01171edc8
ru14 <74753345+ru14@users.noreply.github.com>
2021-04-12 20:38:16 -0700

    modified server.js

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const { Router } = require('express')
 const express = require('express')
+const CustomerManager = require('./lib/CustomerManager.js')
+const Customer = require('./lib/Customer.js')
 const app = express()
+const customerManager = new CustomerManager
+
  
+
+app.get('/', function(req, res){
+  res.render('index',{title:'page', anyArray:[1, 2, 3]});
+});
+app.get('/Customer', function(req, res){
+  res.render('Customer',{output:})
+} )
+app.get('/Tables', function(req, res){
+  const tables = customerManager.getTables()
+  res.render('Tables',res.json(tables))
+} )
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
